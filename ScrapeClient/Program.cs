52cdcfdb4e54ac5b39f600addf113e666b534f23
Brad Kilkenney <bkilkenney@gmail.com>
2016-12-06 16:02:21 -0500

    Initial Commit

---
+++ ScrapeClient/Program.cs
@@ -0,0 +1,23 @@
+﻿using ScrapeLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScrapeClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a url: ");
+            string url = Console.ReadLine();
+
+            Scrape myScrape = new Scrape();
+            string value = myScrape.ScrapeWebPage(url);
+            Console.WriteLine(value);
+            Console.ReadLine();
+        }
+    }
+}
